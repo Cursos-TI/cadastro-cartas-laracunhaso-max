3c459907ec45a41d25b52ccdc1b95e732a80d25b
laracunhaso-max <laracunhaso@gmail.com>
2025-09-20 21:55:12 -0300

    CartasSuperTrunfo.c
    inclusão do nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,18 +5,22 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 /* supertrunfo - os usuários tem que cadastrar 2 cartas, com as seguintes informações: Estado: Uma letra de 'A' a 'H', em char; Código da Carta: letra do estado + número de 01 a 04, char[]
   Nome da Cidade: nome, em char[]; População: número de habitantes, em int; Área: A área da cidade em km2, em float; PIB, em float; Pontos Turísticos: qtde, em int
+  No nivel aventureiro: sistema calcula a Densidade Populacional e o PIB per Capita com base nos dados inseridos.
 */
+
 int main() {
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
 char estado;
 char nomecid[20];
 int pontostur, codcarta, populacao;
-float areakm, pib;
+float areakm, pib, densid, percapita;
+
   // Área para entrada de dados
 printf("Bem vindo ao Super Trunfo!\n");
-printf("Antes de começar a jogar, vamos cadastrar suas cartas: \n");
+printf("Antes de começar a jogar, vamos cadastrar suas cartas. \n");
   
-    printf("Dê um nome à sua cidade: \n");
+    printf("Dê um nome à sua cidade - por favor, não use espaços: \n");
     scanf("%s", &nomecid);
     
     printf("Em qual Estado a %s fica localizada? Escolha uma letra de A a H: \n", nomecid);
@@ -25,39 +29,48 @@ printf("Antes de começar a jogar, vamos cadastrar suas cartas: \n");
     printf("A %s será sua cidade 01 ou 02? \n", nomecid);
     scanf("%d", &codcarta);
 
-    printf("A %s será sua carta %c0%d \n", nomecid, estado, codcarta);
+    printf("A %s é sua carta %c0%d \n", nomecid, estado, codcarta);
 
-    printf("Qual será a população da %s? \n", nomecid);
+    printf("Qual será a população da %s? - por favor, não use sinais de pontuação: \n", nomecid);
     scanf("%d", &populacao);
 
-    printf("Agora, qual será a área construída (em quilometros quadrados)? \n");
+    printf("Agora, qual será a área da cidade (em km²)? \n");
     scanf("%f", &areakm);
 
     printf("E, quantos pontos turísticos a %s tem? \n", nomecid);
     scanf("%d", &pontostur);
 
-    printf("Ótimo! Só falta definir o PIB! Qual o Produto Interno Bruto (em milhões de reais)? \n");
+    printf("Ótimo! Só falta definir o PIB! Qual o Produto Interno Bruto (em milhões de reais)? - por favor, não use sinais de pontuação: \n");
     scanf("%f", &pib);
 
+  // Área para processamento de dados
+    densid = populacao/areakm;
+    percapita = pib/populacao;    
+
+  // Área para exibição dos dados da cidade
   printf("Sua primeira carta foi cadastrada com sucesso! Confira abaixo as informações da %s: \n", nomecid);
       printf("CARTA 1;");
       printf(" Código da Carta: %c0%d \n", estado, codcarta);
       printf("Estado: %c \n", estado);
       printf("Nome da Cidade: %s \n", nomecid);
-      printf("População: %d \n", populacao);
       printf("Área: %.2f km² \n", areakm);
+      printf("População: %d \n", populacao);
+      printf("A densidade populacional é de %.2f habitantes/km² \n", densid);
       printf("PIB: %.2f milhões de reais \n", pib);
+      printf("O PIB per capita da %s é = R$ %.2f/habitante \n", nomecid, percapita);
       printf("Número de Pontos Turísticos: %d \n", pontostur);
 
 printf("\n Pronto para criar sua segunda carta?\n");
-  // Área para exibição dos dados da cidade
+
+  // Área para entrada de dados da carta2
 /* repetição da carta 1, mas alterando as "caixas" de informação para '2'*/
+
 char estado2;
 char nomecid2[20];
 int pontostur2, codcarta2, populacao2;
-float areakm2, pib2;
+float areakm2, pib2, densid2, percapita2;
   
-    printf("Qual nome da segunda cidade? \n");
+    printf("Qual nome da segunda cidade? - por favor, não use espaços: \n");
     scanf("%s", &nomecid2);
     
     printf("Em qual Estado a %s fica localizada? Escolha uma letra de A a H: \n", nomecid2);
@@ -66,19 +79,26 @@ float areakm2, pib2;
     printf("A %s será sua cidade 03 ou 04? \n", nomecid2);
     scanf("%d", &codcarta2);
 
-    printf("A %s será sua carta %c0%d \n", nomecid2, estado2, codcarta2);
+    printf("A %s é sua carta %c0%d \n", nomecid2, estado2, codcarta2);
 
-    printf("Qual será a população da %s? \n", nomecid2);
+    printf("Qual será a população da %s? - por favor, não use sinais de pontuação: \n", nomecid2);
     scanf("%d", &populacao2);
 
-    printf("Agora, qual será a área construída (em quilometros quadrados)? \n");
+    printf("Qual a área da cidade, em km²? - por favor, não use sinais de pontuação: \n");
     scanf("%f", &areakm2);
 
-    printf("E, quantos pontos turísticos a %s tem? \n", nomecid2);
+    printf("Quantos pontos turísticos a %s tem? \n", nomecid2);
     scanf("%d", &pontostur2);
 
-    printf("Ótimo! Só falta definir o PIB! Qual o Produto Interno Bruto (em milhões de reais)? \n");
+    printf("Qual o Produto Interno Bruto (em milhões de reais)?- por favor, não use sinais de pontuação: \n");
     scanf("%f", &pib2);
+
+    // Área para processamento de dados
+   
+    densid2 = populacao2/areakm2;
+    percapita2 = pib2/populacao2; 
+
+    // Área para exibição dos dados da cidade
 
   printf("A segunda carta foi cadastrada com sucesso! Confira abaixo as informações da %s: \n", nomecid2);
       printf("CARTA 2;");
@@ -87,7 +107,9 @@ float areakm2, pib2;
       printf("Nome da Cidade: %s \n", nomecid2);
       printf("População: %d \n", populacao2);
       printf("Área: %.2f km² \n", areakm2);
+      printf("A densidade populacional é de %.2f habitantes/ km² \n", densid2);
       printf("PIB: %.2f milhões de reais \n", pib2);
+      printf("O PIB per capita é = R$ %.2f/habitante \n", percapita2);
       printf("Número de Pontos Turísticos: %d \n", pontostur2);
 
 printf("Tudo pronto para você começar a jogar!");
